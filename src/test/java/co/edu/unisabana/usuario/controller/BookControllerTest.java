1716fac76f41f14f8a2bc0ade73260f39bc48ac8
Richard Guevara <leonvilla6@outlook.com>
2022-09-10 23:12:55 -0500

    Fix text assertion in BookControllerTest
    The test for Given_Book1_When_registerBook_Then_increase_quantity_in_one expected "cantidad actualizada" but endpoint returned "Actualizada cantidad"

--- src/test/java/co/edu/unisabana/usuario/controller/BookControllerTest.java
+++ src/test/java/co/edu/unisabana/usuario/controller/BookControllerTest.java
@@ -36,7 +36,7 @@ public class BookControllerTest extends AbstractTest {
         BookDto dto = new BookDto("Book 1", 2003, "Pedro", "Commercial", "suave");
         restTemplate.postForEntity(PATH_REGISTER_BOOK, dto, BookReponse.class);
         ResponseEntity<BookReponse> result = restTemplate.postForEntity(PATH_REGISTER_BOOK, dto, BookReponse.class);
-        assertEquals("Cantidad actualizada", result.getBody().getData());
+        assertEquals("Actualizada cantidad", result.getBody().getData());
 
     }
 
