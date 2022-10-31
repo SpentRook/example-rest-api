74c7dc114ff2f8b08ad0149d2c665deb05f5ede8
Richard Guevara <leonvilla6@outlook.com>
2022-10-31 07:30:07 -0500

    Update AddBookPort.java

--- src/main/java/co/edu/unisabana/usuario/service/library/port/AddBookPort.java
+++ src/main/java/co/edu/unisabana/usuario/service/library/port/AddBookPort.java
@@ -1,7 +1,5 @@
 package co.edu.unisabana.usuario.service.library.port;
 
-import co.edu.unisabana.usuario.service.library.model.Book;
-
 public interface AddBookPort {
 
     boolean addBook(String name);
